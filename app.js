e389a1c1aee1813e33da436951cbdc63eeb54b3f
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-19 11:13:30 +0530

    test : mock server tested

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var btn = document.querySelector("#btn-translate"); //button
 var inputText = document.querySelector("#txt-input"); //input
 var outputText = document.querySelector("#txt-output"); //output
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslation(text) {
+  return serverURL + "?" + text;
+}
+
+function errorHandler(error) {
+  console.log("error has occured", error);
+  alert("Some error has occured, please try again.");
+}
 
 function clickHandler() {
   //clickEventListener
-  outputText.innerText = "hihitrhi" + inputText.value;
-
-  console.log("event", inputText.value); //taking value of inputText.
+  //   outputText.innerText = "hihitrhi " + inputText.value;
+  var textInput = inputText.value;
+  //  console.log("event", inputText.value); //taking value of inputText.
+  fetch(getTranslation(textInput))
+    .then((response) => response.json())
+    .then((json) => (outputText.innerText = json.contents.translated))
+    .catch(errorHandler);
 }
 
 btn.addEventListener("click", clickHandler);
