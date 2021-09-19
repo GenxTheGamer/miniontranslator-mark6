da09c79af4b4e557812958357c4ff52d28290100
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-19 11:52:39 +0530

    styling completed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btn = document.querySelector("#btn-translate"); //button
 var inputText = document.querySelector("#txt-input"); //input
 var outputText = document.querySelector("#txt-output"); //output
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslation(text) {
-  return serverURL + "?" + text;
+  return serverURL + "?" + "text=" + text;
 }
 
 function errorHandler(error) {
@@ -19,7 +20,10 @@ function clickHandler() {
   //  console.log("event", inputText.value); //taking value of inputText.
   fetch(getTranslation(textInput))
     .then((response) => response.json())
-    .then((json) => (outputText.innerText = json.contents.translated))
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputText.innerText = translatedText;
+    })
     .catch(errorHandler);
 }
 
